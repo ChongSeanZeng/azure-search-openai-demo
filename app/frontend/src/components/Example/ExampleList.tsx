55919beb0fa977796c204aac45276438cc304f1d
ChongSeanZeng <121184498+ChongSeanZeng@users.noreply.github.com>
2024-08-26 09:23:20 +0000

    update frontend questions

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -2,16 +2,12 @@ import { Example } from "./Example";
 
 import styles from "./Example.module.css";
 
-const DEFAULT_EXAMPLES: string[] = [
-    "What is included in my Northwind Health Plus plan that is not in standard?",
-    "What happens in a performance review?",
-    "What does a Product Manager do?"
-];
+const DEFAULT_EXAMPLES: string[] = ["Who is the TIU Director?", "What happens in a Forest management certification?", "What does an FSC auditor do?"];
 
 const GPT4V_EXAMPLES: string[] = [
-    "Compare the impact of interest rates and GDP in financial markets.",
-    "What is the expected trend for the S&P 500 index over the next five years? Compare it to the past S&P 500 performance",
-    "Can you identify any correlation between oil prices and stock market trends?"
+    "Compare the difference of FM and COC certification.",
+    "What is the expected outcome of major nonconformity?",
+    "Can you identify any similarity between multisite and single certificate?"
 ];
 
 interface Props {
